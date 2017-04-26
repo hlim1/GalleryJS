e693a2f56927836218c780deb7409ce9ef11f739
HeuiChan(Terrencce) Lim <hlim1@mail.csuchico.edu>
2017-04-26 11:21:27 -0700

    Updated the shuffleGallery function to the requirements of x24 lab

--- gallery.js
+++ gallery.js
@@ -1,6 +1,16 @@
-
+var chico = ["Chico state", "Upper Bidwell Park", "Monkey face", "One mile", "Downtown"];
+var current = "";
 
 function shuffleGallery()
 {
+  var arraySize = chico.length;
+  var rand = Math.floor(Math.random() * arraySize); // Generates random number between 0 and arraysize.
+  console.log(rand);
 
+  while (current == chico[rand])
+  {
+     rand = Math.floor(Math.random() * arraySize);
+  }
+  current = chico[rand];
+  console.log(current);
 }
