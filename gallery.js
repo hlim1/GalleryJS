a4062e827b65a8fb311afd60384b89ec77dd6f1f
HeuiChan(Terrencce) Lim <hlim1@mail.csuchico.edu>
2017-04-27 14:37:41 -0700

    Updated simple variable array to array with objects

--- gallery.js
+++ gallery.js
@@ -1,4 +1,20 @@
-var chico = ["Chico state", "Upper Bidwell Park", "Monkey face", "One mile", "Downtown"];
+var chico = [
+             {description: "Chico state",
+              picture: "http://www.csuchico.edu/_assets/images/banners/about-desktop.jpg",
+              source: "csuchico.edu"},
+             {description: "Upper Bidwell Park",
+              picture: "http://bidwellpark.org/wp-content/uploads/2014/04/BidwellChicoCreek.jpg", 
+              source: "bidwellpark.org"}, 
+             {description:"Monkey face", 
+              picture: "http://1.bp.blogspot.com/-lYUoGFBMLk0/VBkLkmjVEjI/AAAAAAAACHE/PQ3s_e9-JIY/s1600/MonkeyFace_UpperBidwellPark.jpg",
+              source: "http://trailingahead.blogspot.com/2014/09/monkey-face-in-upper-bidwell-park-east.html"},
+             {description: "One mile",
+              picture: "http://media-cache-ec0.pinimg.com/736x/6e/fc/49/6efc49e16554dc1d67da1b9257680157.jpg", 
+              source: "https://www.pinterest.com/pin/367043438351626082/"},
+             {description: "Downtown",
+              picture: "http://media-cache-ec0.pinimg.com/736x/8c/4a/37/8c4a3760031fe98b3bf3d3bb71add3a7.jpg",
+              source: "https://www.pinterest.com/pin/232920611947846912/"}
+            ];
 var current = "";
 
 function shuffleGallery()
